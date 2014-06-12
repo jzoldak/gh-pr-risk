16e6fd2b95a2da47e2921e44c1ba7baa7a062267
Ben Patterson <bpatterson@edx.org>
2014-06-12 15:11:31 -0400

    PR age rule.

--- gh_pr_risk/age.py
+++ gh_pr_risk/age.py
@@ -3,10 +3,42 @@ Classes for determining risk associated with age of PR
 """
 from base import Rule, Category
 
-class LastCommitAgeRule(Rule):
+class TotalAgeRule(Rule):
     """
     Rule for calculating risk associated the age of
     the most recent commit.
+    """
+    def __init__(self, pr):
+        super(TotalAgeRule, self).__init__(pr)
+        self.name = "Total PR Age"
+        self.description = "How long has the pull request been open?"
+
+    def get_data(self):
+        """
+        Method for obtaining data from github for self.pr.
+        """
+        # TODO
+        DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+        created_date_unicode = self.pr.get('created_at', None)
+        created_date = datetime.datetime.strptime(created_date_unicode, DATETIME_FORMAT)
+        now = datetime.datetime.now()
+        age = (now - created_date).days
+
+        #return age
+        return 8
+
+    def risk(self):
+        """
+        Uses data returned from self.get_data to calculate
+        the risk associated with this feature.
+        """
+        # TODO
+        return 1
+
+class LastCommitAgeRule(Rule):
+    """
+    Rule for calculating risk associated the age of
+    the most recent comment.
     """
     def __init__(self, pr):
         super(LastCommitAgeRule, self).__init__(pr)
@@ -18,31 +50,10 @@ class LastCommitAgeRule(Rule):
         Method for obtaining data from github for self.pr.
         """
         # TODO
-        return None
 
-    def risk(self):
-        """
-        Uses data returned from self.get_data to calculate
-        the risk associated with this feature.
-        """
-        # TODO
-        return 1
+        # Last commit follows a uri like this: https://api.github.com/repos/edx/configuration/commits/0a038c03e842c3d25d29cb2218c5dfbbaf310130
+        # 0a038c03e842c3d25d29cb2218c5dfbbaf310130
 
-class LastCommentAgeRule(Rule):
-    """
-    Rule for calculating risk associated the age of
-    the most recent comment.
-    """
-    def __init__(self, pr):
-        super(LastCommentAgeRule, self).__init__(pr)
-        self.name = "Last Comment Age"
-        self.description = "How long ago was the most recent comment made?"
-
-    def get_data(self):
-        """
-        Method for obtaining data from github for self.pr.
-        """
-        # TODO
         return None
 
     def risk(self):
