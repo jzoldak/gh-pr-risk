9fa9e7302d94722ea6750607da470b188db5f18f
Christine Lytwynec <clytwynec@edx.org>
2014-06-12 13:42:37 -0400

    age.py

---
+++ gh_pr_risk/category/age.py
@@ -0,0 +1,63 @@
+"""
+Classes for determining risk associated with age of PR
+"""
+from base import Rule, Category
+
+class LastCommitAgeRule(Rule):
+    """
+    Rule for calculating risk associated the age of
+    the most recent commit.
+    """
+    def __init__(self, pr):
+        super(LastCommitAgeRule, self).__init__()
+        self.name = "Last Commit Age"
+        self.description = "How long ago was the most recent commit made?"
+
+    def get_data(self):
+        """
+        Method for obtaining data from github for self.pr.
+        """
+        # TODO
+        return None
+
+    def risk(self):
+        """
+        Uses data returned from self.get_data to calculate
+        the risk associated with this feature.
+        """
+        # TODO
+        return None
+
+class LastCommentAgeRule(Rule):
+    """
+    Rule for calculating risk associated the age of
+    the most recent comment.
+    """
+    def __init__(self, pr):
+        super(LastCommentAgeRule, self).__init__()
+        self.name = "Last Comment Age"
+        self.description = "How long ago was the most recent comment made?"
+
+    def get_data(self):
+        """
+        Method for obtaining data from github for self.pr.
+        """
+        # TODO
+        return None
+
+    def risk(self):
+        """
+        Uses data returned from self.get_data to calculate
+        the risk associated with this feature.
+        """
+        # TODO
+        return None
+
+
+class AgeCat(Category):
+    def __init__(self, pr):
+        super(Age, self).__init__()
+        self.rules = [
+            (50, LastCommitAgeRule(self.pr)),
+            (50, LastCommentAgeRule(self.pr)),
+        ]
